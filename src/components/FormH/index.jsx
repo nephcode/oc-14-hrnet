debc539975cf0bd94a20e10d57eacb40311af319
Néphélim <nephcohen@gmail.com>
2024-11-01 13:58:24 +0100

    Form Composer

--- src/components/FormH/index.jsx
+++ src/components/FormH/index.jsx
@@ -1,30 +1,43 @@
-// Import Module ==================================================//
-import scssFormh from './formh.module.scss';
-import states from './states.json';
+// Import Module ===================================================//
 
-// Component ======================================================//
+// Import Data =====================================================//
+import mockData from './states.json';
+
+// import Style ====================================================//
+import scssFormh from './formh.module.scss';
+//import scssForm from '../../scss/form.module.scss';
+// Component =======================================================//
 const FormEmployee = () => {
   return (
     <section className={scssFormh.formh}>
       <form>
         <fieldset>
-          <label htmlFor="firstName">Nom</label>
+          <legend>New Employee</legend>
+          <label htmlFor="firstName">Prénom</label>
           <input type="text" id="firstName" name="firstName" />
-          <label htmlFor="lastName"></label>
+          <label htmlFor="lastName">Nom</label>
           <input type="text" id="lastName" name="lastName" />
           <label htmlFor="dateBirth">Date of Birth</label>
           <input type="date" id="dateBirth" name="dateBirth" />
           <label htmlFor="startMission">Start Date Mission</label>
           <input type="date" id="startMission" name="startMission" />
 
-          <label htmlFor="location">Choisir un lieu</label>
-          <select className={scssFormh.formh}>
-            {states.map((state) => (
-              <option key={state.abbreviation} id={state.abbreviation}>
-                {state.name}
-              </option>
-            ))}
-          </select>
+          <fieldset>
+            <legend>Adresse</legend>
+            <label htmlFor="street">Street</label>
+            <input type="text" id="street" name="street" />
+            <label htmlFor="city">City</label>
+            <input type="text" id="city" name="city" />
+            <label htmlFor="location">State</label>
+            <select className={scssFormh.formh}>
+              {mockData.map((state) => (
+                <option key={state.abbreviation} id={state.abbreviation}>
+                  {state.name}
+                </option>
+              ))}
+            </select>
+          </fieldset>
+          <button type="submit">Submit</button>
         </fieldset>
       </form>
     </section>
