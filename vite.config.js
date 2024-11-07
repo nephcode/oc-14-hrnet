0b28e710011e37055ff7c9db592b8f26225b8da8
Néphélim <nephcohen@gmail.com>
2024-11-07 16:47:39 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: { port: 7014 },
   plugins: [react()],
   css: {
     preprocessorOptions: {
